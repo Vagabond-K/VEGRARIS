13561136993fcbf2482f3f11f7a8596f055f051b
Vagabond K <vagabond-k@naver.com>
2025-09-09 08:54:05 +0900

    문화권에 따른 문자열 포맷으로 인한 오류 개선

--- Vegraris.Wasm/DrawingContext.cs
+++ Vegraris.Wasm/DrawingContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using Vegraris.Drawing;
 
 namespace Vegraris.Wasm
@@ -17,25 +18,33 @@ namespace Vegraris.Wasm
 
         protected override void OnBeginPath(in Point startPoint)
             => stringBuilder?.Append('M')
-            .Append(space).Append(startPoint.X)
-            .Append(space).Append(startPoint.Y);
+            .Append(space).AppendDouble(startPoint.X)
+            .Append(space).AppendDouble(startPoint.Y);
 
         protected override void OnClosePath() => stringBuilder?.Append('Z');
 
         protected override void OnDrawCubicBezier(in Point controlPoint1, in Point controlPoint2, in Point endPoint)
             => stringBuilder?.Append('C')
-            .Append(space).Append(controlPoint1.X)
-            .Append(space).Append(controlPoint1.Y)
-            .Append(space).Append(controlPoint2.X)
-            .Append(space).Append(controlPoint2.Y)
-            .Append(space).Append(endPoint.X)
-            .Append(space).Append(endPoint.Y);
+            .Append(space).AppendDouble(controlPoint1.X)
+            .Append(space).AppendDouble(controlPoint1.Y)
+            .Append(space).AppendDouble(controlPoint2.X)
+            .Append(space).AppendDouble(controlPoint2.Y)
+            .Append(space).AppendDouble(endPoint.X)
+            .Append(space).AppendDouble(endPoint.Y);
 
         protected override void OnDrawLine(in Point endPoint)
             => stringBuilder?.Append('L')
-            .Append(space).Append(endPoint.X)
-            .Append(space).Append(endPoint.Y);
+            .Append(space).AppendDouble(endPoint.X)
+            .Append(space).AppendDouble(endPoint.Y);
 
         protected override string OnGetDrawing() => stringBuilder?.ToString() ?? string.Empty;
+    }
+
+    static class StringBuilderExtensions
+    {
+        private const string doubleFormat = "{0}";
+
+        public static StringBuilder AppendDouble(this StringBuilder stringBuilder, in double value)
+            => stringBuilder.AppendFormat(CultureInfo.InvariantCulture, doubleFormat, value);
     }
 }
